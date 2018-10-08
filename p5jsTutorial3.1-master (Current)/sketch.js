7272b98a182edcb972f5fe8b9ac1f3689db522b0
crunchyflames5773 <42793309+crunchyflames5773@users.noreply.github.com>
2018-10-08 13:08:37 +0800

    Add files via upload

--- p5jsTutorial3.1-master (Current)/sketch.js
+++ p5jsTutorial3.1-master (Current)/sketch.js
@@ -1,18 +1,51 @@
-var buttonNothing;
-var buttonHoverOver;
-var buttonClick;
-var imgCatInMug;
+var buttonYes;
+var buttonNo;
+var buttonMaybe;
+var imgWolf;
 var button2X;
 var button2Y;
 
+var buttonATL
+var buttonPATD
+var buttonFOB
+
+var buttonBut
+var buttonWhy
+var buttonWhyNot
+
+var buttonWhatIs
+var buttonThe
+var buttonMeaningOfLife
+
+var buttonI 
+var buttonAm 
+var buttonYou
+
 function preload()
 {
-	imgCatInMug = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/CatInMug.png');
+	imgWolf = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/wolf.jpg');
 
-	buttonClick = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/buttonClick.png');
-	buttonHoverOver = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/buttonHoverOver.png');
-	buttonNothing = loadImage('https://bleungwpg.github.io/p5jsTutorial3.1/buttonNothing.png');
+	buttonMaybe = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_maybe.png');
+	buttonNo = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_no.png');
+	buttonYes = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_yes.png');
+
+	buttonATL = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_atl.png ');
+	buttonPATD = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_patd.png ');
+	buttonFOB = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_fob.png ');
+
+	buttonBut = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_but.png');
+	buttonWhy = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_why.png');
+	buttonWhyNot = loadImage(' https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_why-not.png');
+
+	buttonWhatIs = loadImage('https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_what-is.png');
+	buttonThe = loadImage('https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_the.png');
+	buttonMeaningOfLife = loadImage('https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_meaning-of-life.png');
+
+	buttonI = loadImage('https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_i.png');
+	buttonAm = loadImage('https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_am.png');
+	buttonYou = loadImage('https://mrleungvsa.github.io/y10-p5js-criterion-a-submission-y10-albrina-chan-a/p5jsTutorial3.1-master (Current)/button_you.png');
 }
+
 
 function setup()
 {
@@ -30,7 +63,7 @@ function draw()
 
 	// Example 1: easy to understand - Start
 
-	image(imgCatInMug,10,150);
+	image(imgWolf,100,150);
 
 	// Example 1: easy to understand - End
 
@@ -43,20 +76,70 @@ function draw()
 
 
 
-	if (mouseX > button2X && mouseX < button2X + 200 && mouseY > button2Y && mouseY < button2Y+50)
+	if (mouseX > button2X && mouseX < button2X + 150 && mouseY > button2Y && mouseY < button2Y+50)
 	{
-		image(buttonHoverOver,button2X,button2Y);
+		image(buttonNo,button2X,button2Y);
 		if (mouseIsPressed)
 		{
-			image(buttonClick,button2X,button2Y);
+			image(buttonMaybe,button2X,button2Y);
 		}
 	}
 	else
 	{
-		image(buttonNothing,button2X,button2Y);
+		image(buttonYes,button2X,button2Y);
 	}
 
+	if(mouseX > 700 && mouseX < 700+100 && mouseY > 0 && mouseY < 0+50)
+	{
+		image(buttonPATD,700,0);
+		if(mouseIsPressed)
+		{
+			image(buttonFOB,700,0);
+		}
+	}
+	else
+	{
+		image(buttonATL,700,0);
+	}
 
+	if(mouseX > 0 && mouseX < 0+100 && mouseY > 560 && mouseY < 560+50)
+	{
+		image(buttonWhy,0,560);
+		if(mouseIsPressed)
+		{
+			image(buttonWhyNot,0,560);
+		}
+	}
+	else
+	{
+		image(buttonBut,0,560);
+	}
+
+	if(mouseX > 650 && mouseX < 650+100 && mouseY > 560 && mouseY < 560+50)
+	{
+		image(buttonThe,710,560);
+		if(mouseIsPressed)
+		{
+			image(buttonMeaningOfLife,600,560);
+		}
+	}
+	else
+	{
+		image(buttonWhatIs,680,560)
+	}
+
+	if(mouseX > 350 && mouseX < 350+100 && mouseY > 100 && mouseY < 100+50)
+	{
+		image(buttonAm,340,100);
+		if(mouseIsPressed)
+		{
+			image(buttonYou,330,100);
+		}
+	}
+	else
+	{
+		image(buttonI,350,100)
+	}
 	// Example 2: less code but harder to understand - End
 
 
